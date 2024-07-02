ddd6f05831acd90b2d32c5540dec4c9847ea42bc
zeynep gul <zeynep1bahadir@gmail.com>
2024-07-02 21:56:20 +0200

    3 jwt error handling callbacks added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
 
 from flask_jwt_extended import JWTManager
@@ -11,10 +11,7 @@ from resources.ingredient import blp as IngredientBlueprint
 from resources.user import blp as UserBlueprint
 from resources.user_extensive import blp as UserExtensiveBlueprint
 
-from schemas import RecipeSchema, IngredientSchema
-
 from db import db
-import models
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -34,6 +31,18 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(128))
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (jsonify({"message": "The token has expired.", "error": "token_expired"}), 401)
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401)
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (jsonify({"description": "Request does not contain an access token.", "error": "authorization_required",}), 401)
 
     with app.app_context():
         db.create_all()
